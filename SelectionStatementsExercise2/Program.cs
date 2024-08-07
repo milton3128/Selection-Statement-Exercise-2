58daab44018e7fc47f55fe45a1cf4cba213f7e70
milton3128 <mannmilton6@gmail.com>
2024-08-07 19:38:50 -0400

    Finished EX2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "Math":
+                    Console.WriteLine("Subject takes a lot of logic!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Subject takes a lot of research!");
+                    break;
+                case "History":
+                    Console.WriteLine("Subject takes a lot of memory, contrast, and comparision!");
+                    break;
+                case "Literature":
+                    Console.WriteLine("Subject takes a lot of reading and comprehension ability!");
+                    break;
+                case "P.E.":
+                    Console.WriteLine("Subject takes a lot of physical endurance and strength!");
+                    break;
+                default:
+                    Console.WriteLine($"WOW! I wanted to take {subject} but I never attended! You are so smart!");
+                    break;
+            }
         }
     }
 }
